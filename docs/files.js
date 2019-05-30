bfcdee4798d4b27b7d0d0daf766a33c7046b588e
jesse <jesse@Jess-LinMint>
2019-05-30 14:18:03 -0400

    Updated Docs

--- docs/files.js
+++ docs/files.js
@@ -1,4 +1,11 @@
 var files =
 [
-    [ "C-Turtle", "dir_42a86653c6886e0887ba2a8832245859.html", "dir_42a86653c6886e0887ba2a8832245859" ]
+    [ "Color.hpp", "Color_8hpp.html", [
+      [ "Color", "structcturtle_1_1Color.html", "structcturtle_1_1Color" ]
+    ] ],
+    [ "Common.hpp", "Common_8hpp.html", "Common_8hpp" ],
+    [ "CTurtle.cpp", "CTurtle_8cpp.html", "CTurtle_8cpp" ],
+    [ "CTurtle.hpp", "CTurtle_8hpp.html", "CTurtle_8hpp" ],
+    [ "Geometry.hpp", "Geometry_8hpp.html", "Geometry_8hpp" ],
+    [ "UserIO.hpp", "UserIO_8hpp.html", "UserIO_8hpp" ]
 ];
